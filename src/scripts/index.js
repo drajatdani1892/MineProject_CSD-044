76bdb97851d89a7e758a7bedb69346af6893b0e6
drajatdani1892 <drajatdani1892@gmail.com>
2021-12-06 20:41:56 +0700

    perubahan dependencies, urutan folder, serta implementasi webpack

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import  brand_icon from '../public/images/icon.svg';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
---
+++ webpack.common.js
@@ -0,0 +1,75 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: '/\.html$/',
+        loader: 'prerender-loader?string'
+      },
+      {
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+        },
+      ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+
+    
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/public/'),
+        to: path.resolve(__dirname, 'dist/'),
+      }],
+    }),
+  ],
+};
