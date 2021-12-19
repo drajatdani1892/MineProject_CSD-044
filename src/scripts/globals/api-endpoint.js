c0169cf41d6a2e49999f807d3f3d9a8028620eac
drajatdani1892 <drajatdani1892@gmail.com>
2021-12-19 19:23:06 +0700

    menghapus beberap hal yang tidak digunakan

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,7 +1,6 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
-  // HOME: `${CONFIG.BASE_URL}events.json?classificationName=music&apikey=${CONFIG.KEY}&size=10`,
   HOME: `${CONFIG.BASE_URL}musicEvent`,
   BLOG: `${CONFIG.BASE_URL}blogcontent`,
   SEARCH: (category, artist) => `${CONFIG.BASE_URL}musicEvent?${category}=${artist}`,
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
-  // KEY: '7mQkloDJM8S283uKOjSccpjEcWuzEVnc',
   BASE_URL: 'https://61b0a1fe3c954f001722a523.mockapi.io/',
   CACHE_NAME: 'Mine App',
-  // BLOG_URL: 'https://61b0a1fe3c954f001722a523.mockapi.io/',
-  //   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
 };
 
 export default CONFIG;
