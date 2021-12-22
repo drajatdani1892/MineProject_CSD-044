7a36170c6213306165090c1992a2d2f3b7a6a26e
drajatdani1892 <drajatdani1892@gmail.com>
2021-12-22 16:50:25 +0700

    fix webmanifest, menambahkan package run-all

--- webpack.common.js
+++ webpack.common.js
@@ -89,28 +89,28 @@ module.exports = {
       crossorigin: 'use-credentials', // can be null, use-credentials or anonymous
       icons: [
         {
-          src: path.resolve('src/public/images/favicon.svg'),
+          src: path.resolve('src/public/images/favicon.png'),
           sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
         },
         {
-          src: path.resolve('src/public/images/favicon.svg'),
+          src: path.resolve('src/public/images/favicon.png'),
           size: '1024x1024',
           purpose: 'maskable',
         },
         {
-          src: path.resolve('src/public/images/favicon.svg'),
+          src: path.resolve('src/public/images/favicon.png'),
           sizes: [120, 152, 167, 180, 1024],
           destination: path.join('icons', 'ios'),
           ios: true,
         },
         {
-          src: path.resolve('src/public/images/favicon.svg'),
+          src: path.resolve('src/public/images/favicon.png'),
           size: 1024,
           destination: path.join('icons', 'ios'),
           ios: 'startup',
         },
         {
-          src: path.resolve('src/public/images/favicon.svg'),
+          src: path.resolve('src/public/images/favicon.png'),
           sizes: [36, 48, 72, 96, 144, 192, 512],
           destination: path.join('icons', 'android'),
         },
